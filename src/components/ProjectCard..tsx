f24e0ee0d4d54b14693bfd67601f01b0cea33d69
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2025-11-27 22:59:35 +0700

    [MODIFY] update ProjectCard component to include link2 for frontend repository

--- src/components/ProjectCard..tsx
+++ src/components/ProjectCard..tsx
@@ -1,6 +1,6 @@
 import type { Project } from "../data/project"
 
-function ProjectCard({ title, description, tech, link }: Project) {
+function ProjectCard({ title, description, tech, link, link2 }: Project) {
     return (
         <div className='bg-[#111] p-6 rounded-xl shadow-md border border-gray-800 hover:border-gray-600 transition'>
             <h4 className='font-semibold text-white text-lg'>{title}</h4>
@@ -8,7 +8,7 @@ function ProjectCard({ title, description, tech, link }: Project) {
             <p className='text-xs mt-3 text-gray-500'>{tech}</p>
             <div className='mt-4'>
                 <a href={link} className='mr-3 text-sm underline text-gray-300 hover:text-white' target="_blank">BE Repo</a>
-                <a href="https://github.com/faridlan/employee-tracker-frontend" className='text-sm underline text-gray-300 hover:text-white' target="_blank">FE Repo</a>
+                <a href={link2} className='text-sm underline text-gray-300 hover:text-white' target="_blank">FE Repo</a>
             </div>
         </div>
     )
--- src/data/project.ts
+++ src/data/project.ts
@@ -3,6 +3,7 @@ export interface Project {
   description: string;
   tech: string;
   link: string;
+  link2: string;
 }
 
 const projects: Project[] = [
@@ -12,6 +13,15 @@ const projects: Project[] = [
       "A system for tracking employee targets and achievements for Bank Galuh Ciamis.",
     tech: "NestJS · React (TS) · SQLite",
     link: "https://github.com/faridlan/employee-tracker-backend",
+    link2: "https://github.com/faridlan/employee-tracker-frontend",
+  },
+  {
+    title: "Meeting Minutes — Bank Galuh Ciamis",
+    description:
+      "A system for write a meeting minute and result meeting of Bank Galuh Ciamis.",
+    tech: "NestJS · React (TS) · SQLite",
+    link: "https://github.com/faridlan/notulen-backend",
+    link2: "https://github.com/faridlan",
   },
 ];
 
